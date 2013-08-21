3f3ba9e98d4485f26a905bf80afd664259af8d36
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-08-22 01:41:30 +0200

    Make it so .org files are in org-mode

--- config/org-mode.el
+++ config/org-mode.el
@@ -1,3 +1,5 @@
+(setq auto-mode-alist (cons '("\\.org$" . org-mode) auto-mode-alist))
+
 (setq org-confirm-babel-evaluate nil)
 
 (setq org-src-fontify-natively t)
@@ -9,11 +11,6 @@
 ;(setq org-todo-keywords '((sequence "TODO" "PLANNED" "|" "DONE")))
 
 (eval-after-load "org"
-  '(progn
-     (org-babel-do-load-languages 'org-babel-load-languages '((emacs-lisp . t)
-                                                              (sh . t)
-                                                              (ruby . t)
-                                                              (python . t)
-                                                              (R . t)
-                                                              (gnuplot . t)))
-     ))
+  '(let ((languages '(emacs-lisp sh ruby python R gnuplot))
+         (helper (lambda (token) `(,token . t))))
+     (org-babel-do-load-languages 'org-babel-load-languages (mapcar helper languages))))
