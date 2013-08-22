d344b5123b7012b751b3c08566e6e5ffcdd4042a
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-08-22 21:59:24 +0200

    Use electric-* instead of describe-*

---
+++ config/ehelp.el
@@ -0,0 +1,12 @@
+(require 'ehelp)
+
+;; Use electric-* instead of describe-* so we can press 'q'
+(substitute-key-definition 'apropos           'electric-apropos           help-map)
+(substitute-key-definition 'command-apropos   'electric-command-apropos   help-map)
+(substitute-key-definition 'view-lossage      'electric-view-lossage      help-map)
+(substitute-key-definition 'describe-key      'electric-describe-key      help-map)
+(substitute-key-definition 'describe-mode     'electric-describe-mode     help-map)
+(substitute-key-definition 'describe-syntax   'electric-describe-syntax   help-map)
+(substitute-key-definition 'describe-function 'electric-describe-function help-map)
+(substitute-key-definition 'describe-variable 'electric-describe-variable help-map)
+(substitute-key-definition 'describe-bindings 'electric-describe-bindings help-map)
