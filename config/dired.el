9ef5abf25505c5d30e1b94ae4d9a8eeece74c1ba
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-08-27 19:02:10 +0200

    Add dired-toggle-dotfiles

--- config/dired.el
+++ config/dired.el
@@ -15,6 +15,19 @@
          (with-current-buffer buf (eval form lexical-binding)(save-buffer))
        (kill-buffer buf))) nil))
 
+;; Taken from http://www.emacswiki.org/emacs/DiredOmitMode
+(defun dired-toggle-dotfiles ()
+  "Show/hide dot-files"
+  (interactive)
+  (when (equal major-mode 'dired-mode)
+    (if (or (not (boundp 'dired-dotfiles-show-p)) dired-dotfiles-show-p) ; if currently showing
+        (progn
+          (set (make-local-variable 'dired-dotfiles-show-p) nil)
+          (dired-mark-files-regexp "^\\\.")
+          (dired-do-kill-lines))
+      (progn (revert-buffer) ; otherwise just revert to re-show
+             (set (make-local-variable 'dired-dotfiles-show-p) t)))))
+
 (eval-after-load 'dired
   '(progn
      (toggle-diredp-find-file-reuse-dir 1)
