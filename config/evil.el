a37759b07de1036c8a422d5c5051b7b635ff9009
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-08-27 21:46:07 +0200

    Use grep keymap when in grep mode and cleanup evil config

--- config/evil.el
+++ config/evil.el
@@ -1,13 +1,7 @@
 (setq evil-default-cursor '(t "white"))
 (setq evil-magic 'very-magic)
 
+(add-to-list 'evil-emacs-state-modes 'grep-mode)
+
 (evil-mode)
 (global-evil-leader-mode)
-
-;(defun my-move-key (keymap-from keymap-to key)
-;     "Moves key binding from one keymap to another, deleting from the old location. "
-;     (define-key keymap-to key (lookup-key keymap-from key))
-;     (define-key keymap-from key nil)
-;     )
-;   (my-move-key evil-motion-state-map evil-normal-state-map (kbd "RET"))
-;   (my-move-key evil-motion-state-map evil-normal-state-map " ")
