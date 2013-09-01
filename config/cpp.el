ffb0cec433f7c70f63373f94b9daebffd2dc3cb2
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-09-01 17:03:10 +0200

    Improve C++ config

--- config/cpp.el
+++ config/cpp.el
@@ -1,2 +1,8 @@
+;; Use the GDB visual debugging mode
+(setq gdb-many-windows t)
+
+;; Treat .h files as C++ files (instead of C)
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; Use the indentation style of "The C++ Programming Language"
 (setq c-default-style "stroustrup")
-;(setq c-basic-offset 4)
