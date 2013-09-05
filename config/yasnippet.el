9642626231bbac6b5813ce79aad6e3fb681b52f2
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-09-05 19:28:55 +0200

    Make it so .yasnippet is in snippet-mode

--- config/yasnippet.el
+++ config/yasnippet.el
@@ -1,4 +1,5 @@
 ;; YASnippet
 (yas/global-mode t)
 (add-to-list 'yas-snippet-dirs (concat config-directory "snippets"))
+(add-to-list 'auto-mode-alist '("\\.yasnippet\\'" . snippet-mode))
 (yas-reload-all)
