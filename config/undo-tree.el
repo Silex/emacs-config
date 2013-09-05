33d47fefe9b00d2e1716be20d3199b9c2d02ddfe
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-09-05 19:28:40 +0200

    Keep region when undoing in region

--- config/undo-tree.el
+++ config/undo-tree.el
@@ -1,3 +1,15 @@
 ;; Undo
 (setq undo-tree-auto-save-history t)
 (setq undo-tree-history-directory-alist `((".*" . ,temporary-file-directory)))
+
+;; Keep region when undoing in region
+(defadvice undo-tree-undo (around keep-region activate)
+  (if (use-region-p)
+      (let ((m (set-marker (make-marker) (mark)))
+            (p (set-marker (make-marker) (point))))
+        ad-do-it
+        (goto-char p)
+        (set-mark m)
+        (set-marker p nil)
+        (set-marker m nil))
+    ad-do-it))
