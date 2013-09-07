4ca0640c2e429570851ed241dcbd6fe6613df356
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-09-07 13:10:55 +0200

    Add comment about ssh-askpass for TRAMP
    TRAMP doesn't work out of the box with ssh keys, it only works with
    passwords. Installing ssh-askpass fixes the issue.

--- config/tramp.el
+++ config/tramp.el
@@ -1,3 +1,5 @@
+;; For TRAMP to work with ssh-key, install the programm ssh-askpass (https://github.com/markcarver/mac-ssh-askpass)
+
 ;(add-to-list 'tramp-default-proxies-alist '(".*" "\`root\'" "/ssh:%h:"))
 
 (defun find-alternative-file-with-sudo ()
