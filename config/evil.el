15d5614dd659bb84bad46c9652f76903c2f1c644
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-09-09 18:57:24 +0200

    Disable evil in git-rebase-mode

--- config/evil.el
+++ config/evil.el
@@ -1,8 +1,8 @@
 (setq evil-default-cursor '(t "white"))
 (setq evil-magic 'very-magic)
 
-(add-to-list 'evil-emacs-state-modes 'grep-mode)
-(add-to-list 'evil-emacs-state-modes 'dired-mode)
+;; List of modes where evil should not be enabled
+(nconc evil-emacs-state-modes '(grep-mode dired-mode git-rebase-mode))
 
 (evil-mode)
 (global-evil-leader-mode)
