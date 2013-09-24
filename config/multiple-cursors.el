1591483666d59fe630dd67118a2b08d30a61b32e
Philippe Vaucher <philippe@stvs.ch>
2013-09-24 10:50:43 +0200

    Add more multiple-cursors autoloads

--- config/multiple-cursors.el
+++ config/multiple-cursors.el
@@ -1,4 +1,6 @@
 (use-package multiple-cursors
   :commands (mc/edit-lines
              mc/mark-next-like-this
+             mc/mark-all-like-this
+             mc/mark-all-like-this-dwim
              mc/mark-all-dwim))
