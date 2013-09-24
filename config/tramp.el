024b49a1f644c9b63458cde6d50074fa55defc1b
Philippe Vaucher <philippe@stvs.ch>
2013-09-24 10:49:54 +0200

    Add TRAMP documentation about SSH + sudo

--- config/tramp.el
+++ config/tramp.el
@@ -1,6 +1,6 @@
 ;; For TRAMP to work with ssh-key, install the programm ssh-askpass (https://github.com/markcarver/mac-ssh-askpass)
 
-;;(add-to-list 'tramp-default-proxies-alist '(".*" "\`root\'" "/ssh:%h:"))
+;; C-x C-f /ssh:you@remotehost|sudo:remotehost:/path/to/file
 
 (defun find-alternative-file-with-sudo ()
   (interactive)
