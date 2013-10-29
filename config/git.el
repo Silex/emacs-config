4276557d96abc2d9a0be5216b9722e361643e73b
Philippe Vaucher <philippe@stvs.ch>
2013-10-29 10:21:49 +0100

    Remove useless magit whitespace helpers

--- config/git.el
+++ config/git.el
@@ -49,23 +49,6 @@
 (defadvice git-commit-abort (after delete-window activate)
   (delete-window))
 
-;; Whitespace
-(defun magit-toggle-whitespace ()
-  (interactive)
-  (if (member "-w" magit-diff-options)
-      (magit-dont-ignore-whitespace)
-    (magit-ignore-whitespace)))
-
-(defun magit-ignore-whitespace ()
-  (interactive)
-  (add-to-list 'magit-diff-options "-w")
-  (magit-refresh))
-
-(defun magit-dont-ignore-whitespace ()
-  (interactive)
-  (setq magit-diff-options (remove "-w" magit-diff-options))
-  (magit-refresh))
-
 ;; C-x C-k to kill file on line
 ;(defun magit-kill-file-on-line ()
 ;  "Show file on current magit line and prompt for deletion."
