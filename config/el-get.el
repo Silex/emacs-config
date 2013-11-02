deb4f0992badfd14a55da7d2bd6c81f3e3e58ee5
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-11-02 21:21:03 +0100

    Configure el-get to work as an installer

--- config/el-get.el
+++ config/el-get.el
@@ -5,7 +5,10 @@
              el-get-self-update
              el-get-cd
              el-get-describe
-             el-get-find-recipe-file)
+             el-get-find-recipe-file
+             el-get-elpa-build-local-recipes
+             el-get-emacswiki-build-local-recipes
+             el-get-list-packages)
 
   :init
   (progn
@@ -20,8 +23,11 @@
     ;; Set up el-get local recipes path
     (add-to-list 'el-get-recipe-path (expand-file-name "recipes/" config-directory))
 
-    ;; Fix broken recipes
-    (push '(:name evil
-                  :build (("make" "all"))
-                  :info nil)
-          el-get-sources)))
+    (defun el-get-read-status-file-force ()
+      "Reimplementation that generates the same structure as `el-get-status-file' from the installed packages."
+      (let* ((files (reverse (directory-files packages-directory t)))
+             (is-directory (lambda (file) (and (file-directory-p file) (not (member file '("." ".."))))))
+             (directories (remove-if-not is-directory files))
+             (packages (mapcar 'file-name-base directories))
+             (packages (mapcar 'intern packages)))
+        (mapcar (lambda (package) `(,package status "installed")) packages)))))
