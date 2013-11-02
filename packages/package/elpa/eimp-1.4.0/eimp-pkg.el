f82a795f73aa4f21c495e59dc96c9031ab4e1207
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-11-02 20:42:38 +0100

    Add ELPA packages

---
+++ packages/package/elpa/eimp-1.4.0/eimp-pkg.el
@@ -0,0 +1,1 @@
+(define-package "eimp" "1.4.0" "Emacs Image Manipulation Package" (quote nil))
---
+++ packages/package/elpa/fullscreen-mode-0.0.1/fullscreen-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "fullscreen-mode" "0.0.1" "fullscreen window support for Emacs" (quote nil))
