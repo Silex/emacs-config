2a18d1d52632691fe6404057cbaad4f617bbec3e
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-11-03 18:07:39 +0100

    Stop using electric-* function and use defadvice instead

--- config/consistency.el
+++ config/consistency.el
@@ -1,20 +1,13 @@
-(require 'ehelp)
-
-;; Use electric-* instead of describe-* so we can press 'q'
-(substitute-key-definition 'apropos           'electric-apropos           help-map)
-(substitute-key-definition 'command-apropos   'electric-command-apropos   help-map)
-(substitute-key-definition 'view-lossage      'electric-view-lossage      help-map)
-(substitute-key-definition 'describe-key      'electric-describe-key      help-map)
-(substitute-key-definition 'describe-mode     'electric-describe-mode     help-map)
-(substitute-key-definition 'describe-syntax   'electric-describe-syntax   help-map)
-(substitute-key-definition 'describe-function 'electric-describe-function help-map)
-(substitute-key-definition 'describe-variable 'electric-describe-variable help-map)
-(substitute-key-definition 'describe-bindings 'electric-describe-bindings help-map)
-
 ;; Make it so man pages have focus when I request them
 (setq Man-notify-method 'aggressive)
 
-;; Give focus to describe-file
-(defadvice describe-file (around focus activate)
-  ad-do-it
-  (other-window 1))
+;; Make it so help windows have focus by default
+(mapc (lambda (function)
+        (ad-add-advice function
+                       '(focus nil t (advice . (lambda () ad-do-it (other-window 1))))
+                       'around
+                       0)
+        (ad-activate function))
+      '(apropos command-apropos view-lossage
+        describe-file describe-bindings describe-mode describe-syntax
+        describe-function describe-variable describe-key))
