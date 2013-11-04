7f465d001ad9baf0dbd575ddaa7c0cac399f1ad7
Philippe Vaucher <philippe@stvs.ch>
2013-11-04 11:43:31 +0100

    Delay exec-path-from-shell-initialize at startup

--- config/exec-path-from-shell.el
+++ config/exec-path-from-shell.el
@@ -1,5 +1,6 @@
 (use-package exec-path-from-shell
-  :commands exec-path-from-shell-initialize)
+  :commands exec-path-from-shell-initialize
+  :idle
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
 
-(when (memq window-system '(mac ns x))
-  (exec-path-from-shell-initialize))
