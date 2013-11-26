117cfa502aa6f686233a306bf6aa9e6308f22f2d
Philippe Vaucher <philippe@stvs.ch>
2013-11-26 13:08:06 +0100

    Make it so ediff doesn't popup a new frame

--- config/ediff.el
+++ config/ediff.el
@@ -4,4 +4,8 @@
              ediff-files
              ediff-directories)
   :init
-  (setq-default ediff-split-window-function 'split-window-horizontally))
+  (progn
+    ;; Split window horizontally by default
+    (setq-default ediff-split-window-function 'split-window-horizontally)
+    ;; Don't popup a new frame when ediffing
+    (setq-default ediff-window-setup-function 'ediff-setup-windows-plain)))
