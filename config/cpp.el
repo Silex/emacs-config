e939c495887956602d756ee3dfeadff9dfa9e390
Philippe Vaucher <philippe.vaucher@gmail.com>
2013-12-10 00:47:17 +0100

    Add c-style based on stroustrup style

--- config/cpp.el
+++ config/cpp.el
@@ -3,8 +3,15 @@
 
   :init
   (progn
+    (c-add-style "silex" '((c-basic-offset . 2)
+                           (c-comment-only-line-offset . 0)
+                           (c-offsets-alist . ((statement-block-intro . +)
+                                               (substatement-open . 0)
+                                               (substatement-label . 0)
+                                               (label . 0)
+                                               (statement-cont . +)))))
     ;; Use the GDB visual debugging mode
     (setq gdb-many-windows t)
 
     ;; Use the indentation style of "The C++ Programming Language"
-    (setq c-default-style "stroustrup")))
+    (setq c-default-style "silex")))
