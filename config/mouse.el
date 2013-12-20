2ddb887d6f8124f1a0e2e2ef7e071d285ac0072e
Philippe Vaucher <philippe@stvs.ch>
2013-12-20 15:13:54 +0100

    Improve middle mouse button selection

--- config/mouse.el
+++ config/mouse.el
@@ -2,3 +2,7 @@
 ;(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
 ;(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
 ;(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+
+(setq mouse-drag-copy-region nil)
+(setq mouse-yank-at-point t)
+(setq x-select-enable-clipboard t)
