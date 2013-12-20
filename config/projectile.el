e363bdfa5936c337e1d0025012ab888f14db8f56
Philippe Vaucher <philippe@stvs.ch>
2013-12-20 15:28:39 +0100

    Make `enter-dired' as projectile's default action

--- config/projectile.el
+++ config/projectile.el
@@ -1,5 +1,8 @@
 (use-package projectile
   :commands (projectile-mode
-             projectile-global-mode))
+             projectile-global-mode)
+  :init
+  (progn
+    (setq projectile-switch-project-action 'enter-dired)))
 
 (projectile-global-mode)
