12a2cf54efde200438c3febb9ca58815b66785a5
Philippe Vaucher <philippe@stvs.ch>
2013-12-23 09:34:04 +0100

    Advice projectile-switch-project to require tramp

--- config/projectile.el
+++ config/projectile.el
@@ -3,6 +3,9 @@
              projectile-global-mode)
   :init
   (progn
-    (setq projectile-switch-project-action 'enter-dired)))
+    (setq projectile-switch-project-action 'enter-dired))
+  :config
+  (defadvice projectile-switch-project (before require-tramp activate)
+    (require 'tramp)))
 
 (projectile-global-mode)
