8decc0cefb0066f79374358a7617a91dfc322e43
Philippe Vaucher <philippe@stvs.ch>
2014-01-03 16:58:17 +0100

    Make `find-alternate-file-with-sudo' work with TRAMP buffers

--- config/tramp.el
+++ config/tramp.el
@@ -14,8 +14,31 @@
   ;; Prefer gvfs for FTP
   (add-to-list 'tramp-gvfs-methods "ftp"))
 
+(defun local-file-name-as-sudo (file-name)
+  "Transforms /foo/bar.ext into /sudo::/foo/bar.ext"
+  (concat "/sudo::" file-name))
+
+(defun tramp-file-name-as-sudo (file-name)
+  "Transforms /scp:user@host:/foo/bar.ext into /ssh:user@host|sudo:host:/foo/bar.ext"
+  (let* ((parts (tramp-dissect-file-name file-name))
+         (host (tramp-file-name-host parts)))
+
+    ;; replace "host:" by "host|sudo:host:"
+    (setq file-name (replace-regexp-in-string (regexp-quote (concat host ":"))
+                                              (concat host "|sudo:" host ":")
+                                              file-name t t))
+
+    ;; replace scp by ssh otherwise sudo doens't work
+    (setq file-name (replace-regexp-in-string "^/scp" "/ssh" file-name))))
+
+(defun buffer-file-name-as-sudo (&optional buffer)
+  "Return BUFFER filename as sudo"
+  (let* ((buffer (or buffer (current-buffer)))
+         (file-name (or (buffer-file-name buffer) dired-directory)))
+    (if (tramp-tramp-file-p file-name)
+        (tramp-file-name-as-sudo file-name)
+      (local-file-name-as-sudo file-name))))
+
 (defun find-alternative-file-with-sudo ()
   (interactive)
-  (let* ((file-name (or buffer-file-name dired-directory))
-         (new-name (concat "/sudo::" file-name)))
-    (find-alternate-file new-name)))
+  (find-alternate-file (buffer-file-name-as-sudo)))
