f79a16585951e575b2290641a63616de5bdb83ba
Philippe Vaucher <philippe@stvs.ch>
2014-01-14 12:24:24 +0100

    Update auto-complete config

--- config/autocomplete.el
+++ config/autocomplete.el
@@ -1,10 +1,19 @@
 (use-package auto-complete
-  :disabled t
+  :commands (global-auto-complete-mode
+             auto-complete-mode
+             auto-complete)
+  :init
+  (progn
+    (setq ac-delay 0.1)
+    (setq ac-auto-show-menu 0.1))
   :config
   (progn
-    (global-auto-complete-mode t)
+    (require 'auto-complete-config)
+    (ac-config-default)
     (add-to-list 'ac-modes 'coffee-mode)
-    (add-to-list 'ac-modes 'haml-mode)))
+    (add-to-list 'ac-modes 'haml-mode))
+  :idle
+  (global-auto-complete-mode))
 
 (use-package auto-complete-clang
   :disabled t)
