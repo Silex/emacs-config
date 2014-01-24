32480ed594ee2f1afa12e848c788b37f31a9ba2d
Philippe Vaucher <philippe@stvs.ch>
2014-01-24 12:15:59 +0100

    Update projectile config

--- config/projectile.el
+++ config/projectile.el
@@ -3,6 +3,7 @@
              projectile-global-mode)
   :init
   (progn
+    (setq projectile-remember-window-configs t)
     (setq projectile-switch-project-action 'enter-dired))
   :config
   (defadvice projectile-switch-project (before require-tramp activate)
