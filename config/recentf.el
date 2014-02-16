eab8775e3437ca42c21663c9b8c74de64e571526
Philippe Vaucher <philippe.vaucher@gmail.com>
2014-02-16 13:10:50 +0100

    Fix & simplify undo-kill-buffer

--- config/recentf.el
+++ config/recentf.el
@@ -10,16 +10,4 @@
 (defun undo-kill-buffer (arg)
   "Re-open the last buffer killed.  With ARG, re-open the nth buffer."
   (interactive "p")
-  (let ((recently-killed-list (copy-sequence recentf-list))
-        (buffer-files-list
-         (delq nil (mapcar (lambda (buf)
-                             (when (buffer-file-name buf)
-                               (expand-file-name (buffer-file-name buf)))) (buffer-list)))))
-    (mapc
-     (lambda (buf-file)
-       (setq recently-killed-list
-	     (delq buf-file recently-killed-list)))
-     buffer-files-list)
-    (find-file
-     (if arg (nth arg recently-killed-list)
-       (car recently-killed-list)))))
+  (find-file (nth (1- arg) recentf-list)))
