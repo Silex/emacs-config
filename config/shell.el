7c10986652b6a370ecc4e245158212e5bc67f21b
Philippe Vaucher <philippe@stvs.ch>
2014-02-25 12:04:27 +0100

    Kill shell processes on exit

--- config/shell.el
+++ config/shell.el
@@ -4,3 +4,15 @@
   (setq explicit-shell-file-name "bash")
   (setq explicit-bash-args '("-c" "export EMACS=; stty echo; bash"))
   (setq comint-process-echoes t))
+
+(use-package comint
+  :defer t
+  :init
+
+  (defun buffer-killer-sentinel (process event)
+    (kill-buffer))
+
+  (defun kill-process-on-exit ()
+    (set-process-sentinel (get-buffer-process (current-buffer)) 'buffer-killer-sentinel))
+
+  (add-hook 'shell-mode-hook 'kill-process-on-exit))
