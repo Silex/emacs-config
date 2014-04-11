ae56fec095f8ef4f0a892bed27c222d6ea8641ce
Philippe Vaucher <philippe@stvs.ch>
2014-04-11 08:45:04 +0200

    Fix C++ style

--- config/cpp.el
+++ config/cpp.el
@@ -2,7 +2,8 @@
   :mode ("\\.h\\'" . c++-mode)
 
   :init
-  (c-add-style "silex" '((c-basic-offset . 2)
+  (c-add-style "silex" '("bsd"
+                         (c-basic-offset . 2)
                          (c-comment-only-line-offset . 0)
                          (c-offsets-alist . ((statement-block-intro . +)
                                              (substatement-open . 0)
