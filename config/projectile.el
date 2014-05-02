191b9dd5427e65c053a4ce0f5501da9a1add1de8
Philippe Vaucher <philippe@stvs.ch>
2014-05-02 09:41:47 +0200

    Enable caching to improve TRAMP performance

--- config/projectile.el
+++ config/projectile.el
@@ -4,6 +4,7 @@
   (setq projectile-remember-window-configs nil)
   (setq projectile-use-git-grep t)
   (setq projectile-switch-project-action 'enter-dired)
+  (setq projectile-enable-caching t)
   :config
   (add-hook 'projectile-mode-hook 'projectile-rails-on)
   (defadvice projectile-switch-project (before require-tramp activate)
