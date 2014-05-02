e37643ccfcf4e8edc7e3b3827845d454b418320c
Philippe Vaucher <philippe@stvs.ch>
2014-05-02 09:41:33 +0200

    Don't remember windows configuration in projectile

--- config/projectile.el
+++ config/projectile.el
@@ -1,7 +1,7 @@
 (use-package projectile
   :defer t
   :init
-  (setq projectile-remember-window-configs t)
+  (setq projectile-remember-window-configs nil)
   (setq projectile-use-git-grep t)
   (setq projectile-switch-project-action 'enter-dired)
   :config
