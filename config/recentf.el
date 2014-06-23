ce9c2f5972e52616161cac1554dc230eda0c6582
Philippe Vaucher <philippe.vaucher@gmail.com>
2014-06-23 15:08:47 +0200

    Store recentf file in .emacs.d

--- config/recentf.el
+++ config/recentf.el
@@ -2,6 +2,7 @@
   :defer t
   :init
   (setq recentf-auto-cleanup 'never)
+  (setq recentf-save-file "~/.emacs.d/recentf")
   (setq recentf-max-menu-items 10000)
   (setq recentf-max-saved-items nil))
 
