338151d68b0045141a841d9c492fd51ae2671d84
Philippe Vaucher <philippe.vaucher@gmail.com>
2014-10-20 10:45:17 +0200

    Refactor keychord

---
+++ config/key-chord.el
@@ -0,0 +1,9 @@
+(use-package key-chord
+  :init
+  (key-chord-define-global "jj" 'ace-jump-word-mode)
+  (key-chord-define-global "jl" 'ace-jump-line-mode)
+  (key-chord-define-global "jk" 'ace-jump-char-mode)
+  (key-chord-define-global "BB" 'ibuffer)
+  (key-chord-define-global "DD" 'dired-jump)
+  (key-chord-define-global "FF" 'ido-find-file)
+  (key-chord-mode +1))
