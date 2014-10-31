293ae3a4d2b32f31a2807080d1317e76e30dbb2b
Philippe Vaucher <philippe@stvs.ch>
2014-10-31 09:14:45 +0100

    Replace rcodetools by seeingisbelieving

--- config/ruby.el
+++ config/ruby.el
@@ -1,5 +1,12 @@
-;; M-x xmp for the win!
 ;; rvm rvmrc create ruby@gemset --ruby-version
+
+(defun seeing-is-believing ()
+  "Replace the current region (or the whole buffer, if none) with the output
+of seeing_is_believing."
+  (interactive)
+  (let ((beg (if (region-active-p) (region-beginning) (point-min)))
+        (end (if (region-active-p) (region-end) (point-max))))
+    (shell-command-on-region beg end "seeing_is_believing" nil 'replace)))
 
 (use-package ruby-mode
   :mode (;; Rake
