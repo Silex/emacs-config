4e63ec8719d8f5568864becf1fe5e692685af955
Philippe Vaucher <philippe@stvs.ch>
2014-10-31 09:21:20 +0100

    Update MELPA url

--- packages.el
+++ packages.el
@@ -2,7 +2,7 @@
 (setq package-enable-at-startup nil)
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("melpa" . "http://melpa.org/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 (package-initialize)
 
