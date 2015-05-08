b15e4f0f874281e69c8c0b04b79176fd30c2413a
Philippe Vaucher <philippe@stvs.ch>
2015-05-08 13:48:56 +0200

    Customize org faces

--- config/org.el
+++ config/org.el
@@ -9,6 +9,15 @@
   (setq org-startup-folded nil)
   (setq org-src-tab-acts-natively t)
   :config
+  ;; Customize faces
+  (custom-set-faces
+   '(org-level-3 ((t (:inherit (outline-4)))))
+   '(org-level-4 ((t (:inherit (outline-5)))))
+   '(org-level-5 ((t (:inherit (outline-6)))))
+   '(org-level-6 ((t (:inherit (outline-7)))))
+   '(org-level-7 ((t (:inherit (outline-8)))))
+   '(org-level-8 ((t (:inherit (outline-3))))))
+
   ;; Enable many languages by default
   (let ((languages '(emacs-lisp sh ruby python R gnuplot))
         (helper (lambda (token) `(,token . t))))
