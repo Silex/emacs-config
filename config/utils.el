cd3400b5be4c7a2142af9324f2e68ac903463ffe
Philippe Vaucher <philippe@stvs.ch>
2015-09-16 15:49:28 +0200

    Add sort-words helper function

--- config/utils.el
+++ config/utils.el
@@ -60,6 +60,17 @@
   (interactive "*")
   (uniquify-all-lines-region (point-min) (point-max)))
 
+(defun sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+Prefixed with negative \\[universal-argument], sorts in reverse.
+
+The variable `sort-fold-case' determines whether alphabetic case
+affects the sort order.
+
+See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\(\\sw\\|\\s_\\)+" "\\&" beg end))
+
 (defun sort-lines-buffer (&optional reverse)
   "Sort lines in buffer alphabetically; REVERSE means descending order.
 The variable `sort-fold-case' determines whether alphabetic case affects
