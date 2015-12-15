dd7f90e2566874f080afd5a9ba1727ba9dba0b02
Philippe Vaucher <philippe@stvs.ch>
2015-12-15 11:07:18 +0100

    Remove marmalade repository

--- packages.el
+++ packages.el
@@ -2,7 +2,6 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
