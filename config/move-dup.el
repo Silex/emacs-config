dfd1a5455cc863ab911a6a68fdca067e3d64a148
Philippe Vaucher <philippe@stvs.ch>
2015-12-15 12:23:53 +0100

    Don't enable move-dup mode in all buffers

--- config/move-dup.el
+++ config/move-dup.el
@@ -1,3 +1,7 @@
 (use-package move-dup
   :config
-  (global-move-dup-mode))
+  (define-global-minor-mode silex/global-move-dup-mode move-dup-mode
+    (lambda ()
+      (when (not (memq major-mode '(org-mode git-rebase-mode)))
+        (move-dup-mode))))
+  (silex/global-move-dup-mode))
