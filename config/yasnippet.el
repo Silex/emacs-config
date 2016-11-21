9a599cad4bb4cdf38bc022dac5845711894b4011
Philippe Vaucher <philippe.vaucher@gmail.com>
2016-11-21 11:38:54 +0100

    Enable yasnippet

--- config/yasnippet.el
+++ config/yasnippet.el
@@ -1,7 +1,7 @@
 (use-package yasnippet
   :ensure t
   :mode ("\\.yasnippet\\'" . snippet-mode)
+  :demand t
   :config
-  (define-key yas-minor-mode-map (kbd "C-c") nil)
   (add-to-list 'yas-snippet-dirs (concat config-directory "snippets"))
   (yas-global-mode))
