af7773d393d4921eef7ecf5191203a19f395973b
Philippe Vaucher <philippe.vaucher@stvs.com>
2017-12-01 11:00:16 +0100

    Use better looking face for markdown code

--- config/markdown.el
+++ config/markdown.el
@@ -1,4 +1,5 @@
 (use-package markdown-mode
   :ensure t
+  :init (custom-set-faces '(markdown-code-face ((t (:background "#2f3029")))))
   :mode (("\\.md\\'" . gfm-mode)
          ("\\.markdown\\'" . markdown-mode)))
