299c041caca48e7f4f6720356a200c765cc33821
Philippe Vaucher <philippe.vaucher@stvs.com>
2018-05-07 10:55:57 +0200

    Improve completion settings

--- config/completion.el
+++ config/completion.el
@@ -1,11 +1,14 @@
+(use-package flx
+  :ensure t)
+
 (use-package smex
   :ensure t
-  :defer t
   :custom
   (smex-save-file "~/.emacs.d/smex-items"))
 
 (use-package ivy
   :ensure t
+  :demand t
   :bind (("C-b" . ivy-switch-buffer)
          ("C-c C-r" . ivy-resume)
          ("C-S-r" . ivy-recentf)
@@ -13,14 +16,20 @@
               ("C-j" . ivy-immediate-done)
               ("RET" . ivy-alt-done))
   :custom
-  (ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  (ivy-re-builders-alist '((swiper . ivy--regex-plus)
+                           (t . ivy--regex-fuzzy)))
   (ivy-use-virtual-buffers t)
+  (ivy-count-format "%d/%d ")
   (enable-recursive-minibuffers t)
+  (ivy-flx-limit 5000)
   :config (ivy-mode))
 
 (use-package counsel
   :ensure t
+  :demand t
   :bind
   ("C-c k" . counsel-ag)
+  :custom
+  (counsel-mode-override-describe-bindings t)
   :config
   (counsel-mode))
