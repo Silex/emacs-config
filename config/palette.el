cc1c891e9a033d80233d00f6e6a08685821166f0
Philippe Vaucher <philippe.vaucher@stvs.com>
2018-06-08 09:08:32 +0200

    Remove some of the unused packages

--- config/palette.el
+++
@@ -1,8 +0,0 @@
-(use-package palette
-  :ensure t
-  :defer t
-  :custom
-  (palette-hex-rgb-digits 2)
-  :config
-  (define-key palette-mode-map (kbd "<mouse-1>") 'palette-pick-background-at-mouse)
-  (define-key palette-mode-map (kbd "<mouse-2>") 'palette-background-at-mouse))
