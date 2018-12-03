be4bb5a09b45795766c6ce4abd402ff672cc1b48
Philippe Vaucher <philippe.vaucher@stvs.com>
2018-12-03 10:50:13 +0100

    Improve evil config

--- config/evil.el
+++ config/evil.el
@@ -1,21 +1,31 @@
 (use-package evil
   :ensure t
   :custom
+  (evil-want-keybinding nil)
   (evil-want-C-d-scroll nil)
   (evil-want-C-i-scroll nil)
   (evil-want-Y-yank-to-eol t)
   (evil-default-cursor '(t "white"))
   (evil-magic 'very-magic)
   (evil-shift-width 2)
+  (evil-want-fine-undo t)
   :config
   (evil-mode)
   (setq evil-emacs-state-modes (append evil-emacs-state-modes '(docker-container-mode docker-image-mode docker-volume-mode docker-network-mode docker-machine-mode))))
+
+(use-package evil-collection
+  :ensure t
+  :after evil
+  :config
+  (evil-collection-init))
 
 (use-package evil-surround
   :ensure t
   :after evil
   :config
-  (global-evil-surround-mode 1))
+  (global-evil-surround-mode 1)
+  (evil-define-key 'visual evil-surround-mode-map "s" 'evil-surround-region)
+  (evil-define-key 'visual evil-surround-mode-map "S" 'evil-Surround-region))
 
 (use-package evil-numbers
   :ensure t
