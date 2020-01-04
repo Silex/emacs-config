d988dd97b28c91632a6618c59f62329024c4ab1d
Philippe Vaucher <philippe.vaucher@gmail.com>
2020-01-04 17:13:22 +0100

    Use package undo-fu

--- config/evil.el
+++ config/evil.el
@@ -67,3 +67,10 @@
   :after evil
   :config
   (evil-goggles-mode))
+
+(use-package undo-fu
+  :demand t
+  :after evil
+  :config
+  (define-key evil-normal-state-map "u" 'undo-fu-only-undo)
+  (define-key evil-normal-state-map "\C-r" 'undo-fu-only-redo))
--- config/undo-tree.el
+++
@@ -1,9 +0,0 @@
-(use-package undo-tree
-  :bind (("C-z" . undo-tree-undo)
-         ("C-S-z" . undo-tree-redo))
-  :custom
-  (undo-tree-mode-lighter "")
-  (undo-tree-auto-save-history t)
-  (undo-tree-history-directory-alist `((".*" . ,temporary-file-directory)))
-  :config
-  (global-undo-tree-mode))
