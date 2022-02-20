6a1e0a8a6f972c8fa223c7d3388c287351ec5a39
Philippe Vaucher <philippe.vaucher@gmail.com>
2022-02-20 11:15:04 +0100

    Bind projectile-rails to `C-c r`

--- config/projectile.el
+++ config/projectile.el
@@ -22,4 +22,5 @@
   :after projectile
   :demand t
   :config
-  (projectile-rails-global-mode))
+  (projectile-rails-global-mode)
+  (define-key projectile-rails-mode-map (kbd "C-c r") 'projectile-rails-command-map))
