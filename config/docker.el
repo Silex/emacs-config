9e0fb8f50dc84967f596b0a53fe71b61234fa869
Philippe Vaucher <philippe.vaucher@gmail.com>
2022-03-20 20:33:51 +0100

    Prefer vterm for docker async buffers

--- config/docker.el
+++ config/docker.el
@@ -3,6 +3,8 @@
   (docker-tramp-use-names t))
 
 (use-package docker
+  :custom
+  (docker-run-async-with-buffer-function 'docker-run-async-with-buffer-vterm)
   :bind ("C-c d" . docker))
 
 (use-package dockerfile-mode)
