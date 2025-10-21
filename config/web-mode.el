c931b652fc215a18befc4ec6807e3bed4e7a4da4
Philippe Vaucher <philippe.vaucher@gmail.com>
2025-10-21 10:18:45 +0200

    Configure web-mode to support erb/deface

--- config/web-mode.el
+++ config/web-mode.el
@@ -1,3 +1,7 @@
 (use-package web-mode
+  :config
+  (setf (cdr (assoc "erb" web-mode-engine-file-regexps))
+    "\\.\\(erb\\|rhtml\\|erb\\.html\\|ecr\\|erb\\.deface\\)\\'")
   :mode (("\\.erb\\'" . web-mode)
+         ("\\.deface\\'" . web-mode)
          ("\\.php\\'" . web-mode)))
