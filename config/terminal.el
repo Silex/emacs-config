0a7f00e4786fd75d3ba33bfe05cbf259732e084d
Philippe Vaucher <philippe.vaucher@gmail.com>
2026-01-12 13:51:20 +0100

    Improve terminal colors

--- config/terminal.el
+++ config/terminal.el
@@ -12,6 +12,24 @@
   (setq exec-path-from-shell-variables '("PATH" "MANPATH" "SSH_AGENT_PID" "SSH_AUTH_SOCK"))
   (exec-path-from-shell-initialize))
 
+(use-package ansi-color
+  :straight nil
+  :custom-face
+  (ansi-color-red ((t (:foreground "#e74856"))))
+  (ansi-color-green ((t (:foreground "#16c60c"))))
+  (ansi-color-yellow ((t (:foreground "#f9f1a5"))))
+  (ansi-color-blue ((t (:foreground "#3b78ff"))))
+  (ansi-color-magenta ((t (:foreground "#d16d9e"))))
+  (ansi-color-cyan ((t (:foreground "#61d6d6"))))
+  (ansi-color-white ((t (:foreground "#ffffff"))))
+  (ansi-color-bright-red ((t (:foreground "#ff5f6a"))))
+  (ansi-color-bright-green ((t (:foreground "#3bdc3b"))))
+  (ansi-color-bright-yellow ((t (:foreground "#fff1b2"))))
+  (ansi-color-bright-blue ((t (:foreground "#6aa2ff"))))
+  (ansi-color-bright-magenta ((t (:foreground "#e08ab3"))))
+  (ansi-color-bright-cyan ((t (:foreground "#7de1e1"))))
+  (ansi-color-bright-white ((t (:foreground "#ffffff")))))
+
 (use-package eat)
 
 ;; sudo snap install cmake && sudo apt install libtool-bin
@@ -21,9 +39,13 @@
 
 (use-package term
   :custom-face
+  (term-color-red   ((t (:foreground "#e74856"))))
   (term-color-cyan  ((t (:foreground "#61d6d6"))))
   (term-color-green ((t (:foreground "#16c60c"))))
-  (term-color-blue  ((t (:foreground "#3b78ff")))))
+  (term-color-yellow ((t (:foreground "#f9f1a5"))))
+  (term-color-blue  ((t (:foreground "#3b78ff"))))
+  (term-color-magenta ((t (:foreground "#d16d9e"))))
+  (term-color-white ((t (:foreground "#ffffff")))))
 
 (use-package comint
   :straight nil
