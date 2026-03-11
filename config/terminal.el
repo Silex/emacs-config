aa0054618197e83c52ee63b6da2949864287da27
Philippe Vaucher <philippe.vaucher@gmail.com>
2026-03-11 11:57:31 +0100

    Fix eat straight recipe assets

--- config/terminal.el
+++ config/terminal.el
@@ -42,6 +42,15 @@
   (ansi-color-bright-white ((t (:foreground "#ffffff")))))
 
 (use-package eat
+  :straight
+  (eat :type git
+       :host codeberg
+       :repo "akib/emacs-eat"
+       :files ("*.el" ("term" "term/*.el") "*.texi"
+               "*.ti" ("terminfo/e" "terminfo/e/*")
+               ("terminfo/65" "terminfo/65/*")
+               ("integration" "integration/*")
+               (:exclude ".dir-locals.el" "*-tests.el")))
   :custom
   (eat-kill-buffer-on-exit t)
   :config
