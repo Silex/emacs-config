799b94c373893abdb36f4c8c15ae3fa16a247c52
Philippe Vaucher <philippe.vaucher@gmail.com>
2026-09-16 13:33:47 +0200

    config: make Y yank to end of line

--- config/evil.el
+++ config/evil.el
@@ -6,13 +6,15 @@
   (evil-want-keybinding nil)
   (evil-want-C-d-scroll nil)
   (evil-want-C-i-scroll nil)
-  (evil-want-Y-yank-to-eol t)
   (evil-default-cursor '(t "white"))
   (evil-magic 'very-magic)
   (evil-shift-width 2)
   (evil-want-fine-undo t)
   (evil-search-module 'evil-search)
   :config
+  ;; Set after loading: evil's deferred custom initialization discards
+  ;; values given through :custom, so Y would keep yanking the whole line.
+  (setopt evil-want-Y-yank-to-eol t)
   (evil-mode)
   (defalias #'forward-evil-word #'forward-evil-symbol))
 
